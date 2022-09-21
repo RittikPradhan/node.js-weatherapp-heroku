40921165a146217858cff7ff60e34231657d5853
RittikPradhan <rittikpradhan5@gmail.com>
2022-09-21 18:27:35 +0530

    build: port updated

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const forecast = require("./utils/forecast");
 // console.log(__filename);
 
 const app = express();
-const port = process.env.POR || 3000; //POR = Port (heroku) || fallbackvalue (port 3000 - local)
+const port = process.env.PORT || 3000; //POR = Port (heroku) || fallbackvalue (port 3000 - local)
 
 //Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, "../public");
